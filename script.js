4b3e20f3bfb7099892f374abe524d72ca82f30f5
Ashish Kumar Jha <letscodeit.ashish@gmail.com>
2024-05-06 12:21:38 +0530

    Add files via upload
    Files uploaded

---
+++ script.js
@@ -0,0 +1,24 @@
+async function getMatchData() {
+
+    return await fetch("https://api.cricapi.com/v1/currentMatches?apikey=b20abfa7-963d-4506-bceb-91d9c4011050&offset=0")
+        .then(data => data.json())
+        .then(data => {
+            if (data.status != "success")return;
+
+            const matchesList = data.data;
+
+            if(!matchesList)return [];
+        
+            const relevantData = matchesList.filter(match => match.series_id == "76ae85e2-88e5-4e99-83e4-5f352108aebc").map(match => `${match.name}, ${match.status}`);
+
+            console.log({relevantData});
+
+            document.getElementById("matches").innerHTML = relevantData.map(match => `<li>${match} </li>`).join('');
+
+            return relevantData;
+
+        })
+        .catch(e => console.log(e));
+}
+
+getMatchData();
